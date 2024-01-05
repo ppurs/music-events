10575e4050e8614a6c18c9ba40f3ac80d6d150fb
ppurs <paulinaaa523@gmail.com>
2024-01-05 23:50:24 +0100

    fix: refresh roles directive on logout

--- music-events-ui/src/app/directives/for-roles.directive.ts
+++ music-events-ui/src/app/directives/for-roles.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 import { AuthService } from '../core/auth/services/auth.service';
 import { Role } from '../core/auth/models/role';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[forRoles]'
 })
-export class ForRolesDirective {
+export class ForRolesDirective implements OnInit {
 
   roles: Role[];
+  loginSubscription?: Subscription;
 
   @Input()
   set forRoles( inRoles: Role[] | Role ) {
@@ -18,16 +20,6 @@ export class ForRolesDirective {
     else {
       this.roles = [];
     }
-
-    this.authService.getUserRoles$().subscribe(
-      userRoles => {
-        if (userRoles && !this.roles.some(val => userRoles.includes(val) )) {
-          this.viewContainer.clear();
-        } else {
-          this.viewContainer.createEmbeddedView(this.templateRef);
-        }
-      }
-    );
   }
 
   constructor(
@@ -35,6 +27,24 @@ export class ForRolesDirective {
     private templateRef: TemplateRef<any>,
     private authService: AuthService) {
     this.roles = []
+  }
+
+  ngOnInit(): void {
+    this.loginSubscription = this.authService.isLoggedIn$.subscribe( () => {
+      this.authService.getUserRoles$().subscribe(
+        userRoles => {
+          if (userRoles && !this.roles.some(val => userRoles.includes(val) )) {
+            this.viewContainer.clear();
+          } else {
+            this.viewContainer.createEmbeddedView(this.templateRef);
+          }
+        }
+      );
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.loginSubscription?.unsubscribe()
   }
 
 }
