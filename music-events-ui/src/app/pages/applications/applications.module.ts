d46f798fe24122893b502285f938d607216d36a8
ppurs <paulinaaa523@gmail.com>
2024-01-13 14:16:53 +0100

    add application details

--- music-events-ui/src/app/pages/applications/applications.module.ts
+++ music-events-ui/src/app/pages/applications/applications.module.ts
@@ -1,16 +1,18 @@
+import { ApplicationDetailsDialogComponent } from './components/application-details-dialog/application-details-dialog.component';
+import { ApplicationsFilterComponent } from './components/applications-filter/applications-filter.component';
+import { ApplicationsListItemComponent } from './components/applications-list-item/applications-list-item.component';
 import { ApplicationsPageComponent } from './components/applications-page/applications-page.component';
 import { ApplicationsRoutingModule } from './applications-routing.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ApplicationsFilterComponent } from './components/applications-filter/applications-filter.component';
-import { ApplicationsListItemComponent } from './components/applications-list-item/applications-list-item.component';
 
 @NgModule({
   declarations: [
     ApplicationsPageComponent,
     ApplicationsFilterComponent,
-    ApplicationsListItemComponent
+    ApplicationsListItemComponent,
+    ApplicationDetailsDialogComponent
   ],
   imports: [
     ApplicationsRoutingModule,
